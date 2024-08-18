95d8e6a7429bcde0907711724e0ab78cb1e91e90
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-08-18 21:15:52 +0200

    Learning functions

--- Day_8/main.py
+++ Day_8/main.py
@@ -5,3 +5,15 @@ def do_three_things():
 
 
 do_three_things()
+
+# using params
+
+
+def do_three_things(name):
+    print(f"What's up {name}...")
+    print("Do the dishes")
+    print("Take out the trash")
+    print("Watch TV")
+
+
+do_three_things("Amar")
