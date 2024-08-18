079f9f2d8fd07fc02f50212fd3f114e626d9543c
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-08-18 22:07:00 +0200

    Creating caesar's cypher

---
+++ Day_8/caesar_cypher.py
@@ -0,0 +1,41 @@
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
+
+
+def encrypt(original_text, shift_amount):
+    encrypted_text = ""
+    for char in original_text.lower():
+        index = letters.index(char)
+        encrypted_text += letters[index + shift_amount % len(letters)]
+    print(encrypted_text)
+
+
+encrypt("Amar", 1)
--- Day_8/main.py
+++ Day_8/main.py
@@ -1,19 +1,28 @@
-def do_three_things():
-    print("Do the dishes")
-    print("Take out the trash")
-    print("Watch TV")
+# def do_three_things():
+#     print("Do the dishes")
+#     print("Take out the trash")
+#     print("Watch TV")
 
 
-do_three_things()
+# do_three_things()
 
-# using params
-
-
-def do_three_things(name):
-    print(f"What's up {name}...")
-    print("Do the dishes")
-    print("Take out the trash")
-    print("Watch TV")
+# # using params
 
 
-do_three_things("Amar")
+# def do_three_things(name):
+#     print(f"What's up {name}...")
+#     print("Do the dishes")
+#     print("Take out the trash")
+#     print("Watch TV")
+
+
+# do_three_things("Amar")
+
+
+def greet_with(name, location):
+    print(f"Hello {name}!")
+    print(f"How's the weather in {location}?")
+
+
+greet_with("Amar", "Novi Pazar")
+greet_with(name="Murga", location="Serbia")
