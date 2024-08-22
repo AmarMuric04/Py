0040f93eb79622cc2765d059c21cfde212eb9ab6
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-08-22 04:01:01 +0200

    Done with the silent auction

--- Day_9/blind_auction.py
+++ Day_9/blind_auction.py
@@ -1,18 +1,18 @@
-print(
-    '''
-                         ___________
-                         \         /
-                          )_______(
-                          |"""""""|_.-._,.---------.,_.-._
-                          |       | | |               | | ''-.
-                          |       |_| |_             _| |_..-'
-                          |_______| '-' `'---------'` '-'
-                          )"""""""(
-                         /_________\\
-                         `'-------'`
-                       .-------------.
-                      /_______________\\'''
-)
+from logo import logo
+
+print(logo)
+
+
+def find_highest_bidder(bidders):
+    winner = ""
+    highest_bid = 0
+    for bidders_key in bidders:
+        bid_amount = bidders[bidders_key]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidders_key
+    print(f"The person who bid the most is {winner} with a bid of {highest_bid}$")
+
 
 new_bidder = True
 blind_auction = {}
@@ -20,10 +20,11 @@ while new_bidder:
     name = input("What's your name?: ")
     bid = int(input("What's your bid?: $"))
     blind_auction[name] = bid
-    new_bidder = input("Is there another bidder? (y/n): ") == "y"
+    new_bidder = input("Is there another bidder? (y/n): ").lower() == "y"
     print("\n" * 100)
 
-max_key = max(blind_auction, key=blind_auction.get)
-print(
-    f"The person who bid the most is {max_key} with a bid of {blind_auction[max_key]}$"
-)
+find_highest_bidder(blind_auction)
+# max_key = max(blind_auction, key=blind_auction.get)
+# print(
+#     f"The person who bid the most is {max_key} with a bid of {blind_auction[max_key]}$"
+# )
