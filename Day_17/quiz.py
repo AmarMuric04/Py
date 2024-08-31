e840552a8f31d055356f6a801149bde05b140318
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-08-31 23:52:17 +0200

    Creating a list of questions

---
+++ Day_17/quiz.py
@@ -0,0 +1,11 @@
+from data import question_data
+from question_model import Question
+
+question_bank = []
+
+for q in question_data:
+    question_bank.append(Question(q["text"], q["answer"]))
+
+
+for q in question_bank:
+    print(q.text, q.answer)
