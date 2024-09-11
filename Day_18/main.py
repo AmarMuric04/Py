a8819947e05f4f113e78bac6213877afda26a75e
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-09-11 23:56:09 +0200

    Drawing a square with the turtle

--- Day_18/main.py
+++ Day_18/main.py
@@ -3,9 +3,16 @@ from turtle import Screen, Turtle
 turtle = Turtle()
 turtle.shape("turtle")
 turtle.color("red")
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+
+for _ in range(4):
+    turtle.right(90)
+    turtle.forward(100)
 
 screen = Screen()
 screen.exitonclick()
