77985f6294533486d83b411e88795b83c2885eb9
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-09-12 23:41:32 +0200

    funny design using turtle

--- Day_18/main.py
+++ Day_18/main.py
@@ -1,5 +1,9 @@
 from turtle import Screen, Turtle
 
+# from turtle import *
+# import turtle as tur
+
+
 turtle = Turtle()
 turtle.shape("turtle")
 turtle.color("red")
@@ -14,5 +18,23 @@ for _ in range(4):
     turtle.right(90)
     turtle.forward(100)
 
+turtle.forward(100)
+turtle.forward(100)
+
+for _ in range(3):
+    turtle.right(90)
+    turtle.forward(100)
+
+turtle.forward(100)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.forward(100)
+
+
 screen = Screen()
 screen.exitonclick()
+
+# import heroes
