7614e1a394eff52195105e333905bb09f53a992d
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-09-13 23:59:05 +0200

    turtle

--- Day_18/main.py
+++ Day_18/main.py
@@ -38,3 +38,43 @@ screen = Screen()
 screen.exitonclick()
 
 # import heroes
+from turtle import Screen, Turtle
+
+# from turtle import *
+# import turtle as tur
+
+
+turtle = Turtle()
+turtle.shape("turtle")
+turtle.color("red")
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+
+
+for _ in range(4):
+    turtle.right(90)
+    turtle.forward(100)
+
+turtle.forward(100)
+turtle.forward(100)
+
+for _ in range(3):
+    turtle.right(90)
+    turtle.forward(100)
+
+turtle.forward(100)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.left(90)
+turtle.forward(100)
+turtle.forward(100)
+
+
+screen = Screen()
+screen.exitonclick()
+
+# import heroes
