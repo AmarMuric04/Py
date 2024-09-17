577f209de9c0d277762826fbbe67c4d8068d4ca9
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-09-17 23:24:58 +0200

    Created etch-a-sketch

--- Day_19/main.py
+++ Day_19/main.py
@@ -8,6 +8,32 @@ def move_forwards():
     turtle.forward(10)
 
 
+def move_backwards():
+    turtle.backward(10)
+
+
+def turn_left():
+    turtle.left(10)
+
+
+def turn_right():
+    turtle.right(10)
+
+
+def clear():
+    turtle.speed("fastest")
+    turtle.penup()
+    turtle.goto(0, 0)
+    turtle.setheading(0)
+    turtle.clear()
+    turtle.pendown()
+    turtle.speed("normal")
+
+
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=turn_left)
+screen.onkey(key="d", fun=turn_right)
+screen.onkey(key="c", fun=clear)
 screen.exitonclick()
