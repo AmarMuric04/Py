36988f1c51efc25fce54ce2a90c6a97acb5f3126
AmarMuric04 <113850369+AmarMuric04@users.noreply.github.com>
2024-09-18 00:26:12 +0200

    Turtle race completed

--- Day_19/turtle_race/main.py
+++ Day_19/turtle_race/main.py
@@ -3,19 +3,40 @@ from turtle import Screen, Turtle
 
 turtles = [Turtle() for _ in range(5)]
 screen = Screen()
+screen.setup(500, 400)
+
+colors = ["red", "orange", "yellow", "green", "blue"]
 
 for index, turtle in enumerate(turtles):
     turtle.penup()
     turtle.shape("turtle")
-    turtle.goto(-500, index * 50)
+    turtle.goto(-225, index * 25)
+    turtle.color(colors[index])
 
 
 def move(t):
-    t.forward(random.randint(0, 20))
+    global bet
+    if t.fillcolor() == bet:
+        t.forward(random.randint(4, 20))
+    else:
+        t.forward(random.randint(0, 20))
 
 
-while not any(turtle.xcor() >= 500 for turtle in turtles):
+bet = screen.textinput(
+    title="Make your bet!", prompt="Which turtle will win the race? Enter a color:"
+)
+
+race_over = False
+while not race_over:
     for turtle in turtles:
         move(turtle)
+        if turtle.xcor() >= 225:
+            race_over = True
+            print(f"The {turtle.fillcolor()} turtle won the race!")
+            if bet.lower() != turtle.fillcolor().lower():
+                print("You were wrong!")
+            else:
+                print("Boom! You got it right!")
+            break
 
 screen.exitonclick()
